16a5b75ce8e282b7feea03c9cd94452f2e27112c
Loris Lucido <loris.lucido@zaclys.net>
2018-01-04 15:53:36 +0100

    up

--- i3blocks/meminfo_lexer/memory.c
+++ i3blocks/meminfo_lexer/memory.c
@@ -15,8 +15,9 @@ int main()
     yyin = fopen("/proc/meminfo", "r");
     yylex();
 
-    double used_mem = (total_mem - free_mem) / 1024 / 1024;
+    // double used_mem = (total_mem - free_mem) / 1024 / 1024;
     int percent = ((total_mem - free_mem) / total_mem) * 100;
+    free_mem = free_mem / 1024 / 1024;
 
     char *color;
     if (percent > 85)
@@ -27,7 +28,7 @@ int main()
         color = COL_SAFE;
 
     /* i3blocks pango format */
-    printf("<span foreground=\"%s\">%.1fG</span>\n", color, used_mem);
-    printf("<span foreground=\"%s\">%.1fG</span>\n", color, used_mem);
+    printf("<span foreground=\"%s\">%.1fG</span>\n", color, free_mem);
+    printf("<span foreground=\"%s\">%.1fG</span>\n", color, free_mem);
     return 0;
 }
